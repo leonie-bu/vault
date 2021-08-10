666b48f5fa75bdca9b6662d14d9c860b5f664c41
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-10 18:23:41 +0200

    ✨ Add functionality for route/api/credentials

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
+
+app.get('/api/credentials', async (_request, response) => {
+  response.send(await readCredentials());
+});
 
 app.get('/', (_request, response) => {
   response.send('Hello World!');
