a16f28fd171ef6c324e33f8822b21a19025a38f2
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-10 15:09:37 +0200

    🎨 Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
-console.log('Hello Sunshine!');
+import { readCredentials } from './utils/credentials';
+
+console.log(readCredentials);
+
+// import { readFile } from 'fs';
+
+// // const fs = require('fs'); --> require ist veraltet
+
+// readFile('./db.json', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// import { readFile } from 'fs/promises';
+
+// async function readPasswords() {
+//   const db = await readFile('src/db.json', 'utf-8');
+//   console.log(db);
+// }
+// readPasswords();
+
+// await readfile gibt mir ein promise zurück.
+// bis ich das promise bekomme muss ich warten.
