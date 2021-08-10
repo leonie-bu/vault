22b9f87a067f929802adf7c4750f88d876b66e1b
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-10 15:07:45 +0200

    ✨ Create readCredential function

---
+++ src/utils/credentials.ts
@@ -0,0 +1,9 @@
+import { readFile } from 'fs/promises';
+import { DB, Credential } from '../types';
+
+export async function readCredentials(): Promise<Credential[]> {
+  const response = await readFile('src/db.json', 'utf-8');
+  const db: DB = JSON.parse(response);
+  const credentials = db.credentials;
+  return credentials;
+}
