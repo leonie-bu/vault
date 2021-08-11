9390b62777c376060d91c94b20431f356e1d8b54
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-11 12:04:00 +0200

    ✨ Create route to get single credential

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import express from 'express';
-import { readCredentials } from './utils/credentials';
+import { getCredential, readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
 
 app.get('/api/credentials', async (_request, response) => {
-  response.send(await readCredentials());
+  try {
+    response.send(await readCredentials());
+  } catch (error) {
+    console.error(error);
+    response.status(500).send(`Internal server error`);
+  }
+});
+
+app.get('/api/credentials/:service', async (request, response) => {
+  const { service } = request.params;
+  try {
+    const credential = await getCredential(service);
+    response.status(200).json(credential);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find service: ${service}`);
+  }
 });
 
 app.get('/', (_request, response) => {
@@ -13,5 +29,5 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}!`);
+  console.log(`Server is listening on port http://localhost:${port}`);
 });
