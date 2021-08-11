2fcdffd6ab4f061266f9f85ef22260183e3e3bba
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-11 12:04:31 +0200

    ✏️ Fix backtick typos

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -11,11 +11,11 @@ export async function readCredentials(): Promise<Credential[]> {
 export async function getCredential(service: string): Promise<Credential> {
   const credentials = await readCredentials();
   const credential = credentials.find(
-    (credential) => credential.service === service
+    (credential) => credential.service.toLowerCase() === service.toLowerCase()
   );
 
   if (!credential) {
-    throw new Error('No credential found for services: ${service}');
+    throw new Error(`No credential found for services: ${service}`);
   }
   return credential;
 }
