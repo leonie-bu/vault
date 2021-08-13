50b5a95763299b8f7e25e3a7e547c59e85d46068
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-13 11:52:38 +0200

    🎨 Specify masterpassword

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,13 +1,13 @@
 import { readFile } from 'fs/promises';
 
 export async function validateMasterpassword(
-  password: string
+  masterpassword: string
 ): Promise<boolean> {
   // read hashed masterpassword from .password
   const hashedMasterpassword = await readFile('.password', 'utf-8');
 
   // hash password argument
-  const hashedPassword = CryptoJS.SHA256(password).toString();
+  const hashedPassword = CryptoJS.SHA256(masterpassword).toString();
 
   // compare hashedMasterpassword with password and return the outcome
   return hashedMasterpassword === hashedPassword;
