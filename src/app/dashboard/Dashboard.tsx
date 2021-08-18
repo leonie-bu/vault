c17f2340964ac7b9d2142f9577b174ab51b8e509
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-18 17:20:42 +0200

    ✨ Create Dashboard function in Dashboard.tsx

---
+++ src/app/dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from 'Dashboard.module.css';
+
+export default function Dashboard(): JSX.Element {
+  return (
+    <>
+      <h1>Vault</h1>
+    </>
+  );
+}
