d0def3bd890fcd167521249d6fa2159e1887c598
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-18 18:13:40 +0200

    🎨 Add body to App function

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './App.module.css';
-import Dashboard from './dashboard/Dashboard';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App(): JSX.Element {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
