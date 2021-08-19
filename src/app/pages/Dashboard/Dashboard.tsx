c931f1ab6d8d319fc406c48fabab01d0191611fc
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-19 10:00:42 +0200

    🎨 Use main element and remove react fragments

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -3,12 +3,10 @@ import styles from './Dashboard.module.css';
 
 export default function Dashboard(): JSX.Element {
   return (
-    <>
-      <div className={styles.container}>
-        <h1 className={styles.heading}>Vault</h1>
-        <p className={styles.text}>Find your passwords</p>
-        <input type="text" className={styles.input}></input>
-      </div>
-    </>
+    <main className={styles.container}>
+      <h1 className={styles.heading}>Vault</h1>
+      <p className={styles.text}>Find your passwords</p>
+      <input type="text" className={styles.input}></input>
+    </main>
   );
 }
