ce384e1af93b1287a2e0b3b2a72a92a4d2f0eace
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-19 16:35:00 +0200

    ✨ Update useEffect function to fetch data from backend

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Dashboard.module.css';
 import { Link } from 'react-router-dom';
+import type { Credential } from '../../../types';
 
 export default function Dashboard(): JSX.Element {
+  const [credentials, setCredentials] = useState<Credential[]>([]);
+
+  // inside useEffect we want to
+  // fetch credentials, then setCredentials to fetched credentials
+  // callback fn, deps array
+
+  useEffect(() => {
+    async function fetchCredentials() {
+      const response = await fetch(`/api/credentials/`, {
+        headers: {
+          Authorization: 'Blumen',
+        },
+      });
+      const credentials = await response.json();
+      setCredentials(credentials);
+    }
+    fetchCredentials();
+  }, []);
+
   return (
     <main className={styles.container}>
       <h1>Vault</h1>
-      <Link to="/password/:service">Pa</Link>
       <p>Find your passwords</p>
-      <input type="text"></input>
+      <Link to="/password/Leonie">Service</Link>
+      <input />
+      {credentials &&
+        credentials.forEach((credential) => console.log(credential))}
     </main>
   );
 }
