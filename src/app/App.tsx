208162c9532cd1e9a31d2c9ad49f072d3dec15dd
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-19 11:31:05 +0200

    ✨ Create first path in App.tsx and Dashboard.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Dashboard from './pages/Dashboard/Dashboard';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-    <div>
-      App
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/Leonie">
+          <p>Leonie</p>
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Dashboard.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard(): JSX.Element {
   return (
     <main className={styles.container}>
       <h1>Vault</h1>
+      <Link to="/Leonie">Hallo</Link>
       <p>Find your passwords</p>
       <input type="text"></input>
     </main>
