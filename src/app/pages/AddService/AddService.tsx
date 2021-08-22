4084f622fef779ce7aef721321e54dcadf85c789
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-22 20:10:44 +0200

    ✨ Create function AddService & update html structure

--- src/app/pages/AddService/AddService.tsx
+++ src/app/pages/AddService/AddService.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AddService.module.css';
 
 export default function AddService(): JSX.Element {
+  const [service, setService] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [masterpassword, setMasterpassword] = useState<string>('');
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const newCredential = {
+      service: service,
+      name: name,
+      password: password,
+    };
+    await fetch('/api/credentials', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: masterpassword,
+      },
+      body: JSON.stringify(newCredential),
+    });
+  }
+
   return (
     <main className={styles.container}>
       <h1>Vault</h1>
@@ -9,10 +31,40 @@ export default function AddService(): JSX.Element {
         Fill in the following fields to save your passwords for a certain
         service
       </h3>
-      <input className={styles.input} placeholder={'Enter service'} />
-      <input placeholder={'Enter name or email address'} />
-      <input placeholder={'Enter password'} />
-      <input placeholder={'Enter masterpassword'} />
+      <form
+        className={styles.container}
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <input
+          placeholder={'Enter service'}
+          type="text"
+          value={service}
+          onChange={(event) => setService(event.target.value)}
+          required
+        />
+        <input
+          placeholder={'Enter name or email address'}
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+        <input
+          placeholder={'Enter password'}
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+        <input
+          placeholder={'Enter masterpassword'}
+          type="password"
+          value={masterpassword}
+          onChange={(event) => setMasterpassword(event.target.value)}
+          required
+        />
+        <input className={styles.submit} value={'Submit'} type="submit" />
+      </form>
     </main>
   );
 }
